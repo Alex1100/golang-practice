550f4c47535845fd400389c3c30cb656505910f8
Alexander Aleksanyan <aleksanyanalek@gmail.com>
2018-05-30 16:52:54 -0700

    add deck-of-cards

---
+++ deck-of-cards/main.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+var shoe [][]string
+
+var cardDefaults = []string{
+	"A",
+	"2",
+	"3",
+	"4",
+	"5",
+	"6",
+	"7",
+	"8",
+	"9",
+	"10",
+	"J",
+	"Q",
+	"K",
+}
+
+func shuffleCards(d []string) []string {
+	var z, i int
+	var t string
+	z = len(d)
+	r := rand.New(rand.NewSource(time.Now().Unix()))
+
+	for z > 0 {
+		i = random(r, 1, 52)
+		z = z - 1
+		t = d[z]
+		d[z] = d[i]
+		d[i] = t
+	}
+
+	return d
+}
+
+func createDeck() []string {
+	deck := make([]string, 0)
+
+	for _, c := range cardDefaults {
+		card := string(c)
+		variants := []string{
+			card + "♠",
+			card + "♥",
+			card + "♦",
+			card + "♣",
+		}
+
+		deck = append(deck, variants...)
+	}
+	deck = shuffleCards(deck)
+	return deck
+}
+
+func random(r *rand.Rand, min int, max int) int {
+	return r.Intn(max-min) + min
+}
+
+func createShoe(s int) [][]string {
+	shoe := make([][]string, 0)
+	for i := 0; i < s; i++ {
+		shoe = append(shoe, createDeck())
+	}
+	return shoe
+}
+
+func main() {
+	r := rand.New(rand.NewSource(time.Now().Unix()))
+	shoeSize := random(r, 2, 9)
+	fmt.Println("SHOE SIZE IS: ", shoeSize)
+	shoe := createShoe(shoeSize)
+
+	fmt.Println("FIRST CARD IS: ", shoe[0][0])
+	fmt.Println("\n\nSHOE IS: ", shoe)
+	fmt.Println("\n\nFIRST DECK IN THE SHOE IS: ", shoe[0], len(shoe[0]))
+}
